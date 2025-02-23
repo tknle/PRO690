60fe62fb753234b2a646c7fc1f025e7c78d602fb
John Doe <johndoe@example.com>
2025-02-23 15:09:11 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectMVCExample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
