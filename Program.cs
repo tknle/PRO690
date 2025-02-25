e60c639e0dbb4d98fef3c3bbfafafa30b89a7d5a
John Doe <johndoe@example.com>
2025-02-25 00:01:05 -0500

    upd secret in github

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
--- obj/Debug/net8.0/ProjectMVCExample.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectMVCExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a64b81aa3adb9c748290c9d3d834d6deb5497b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91cb68ccd8e89ed014571f70de3d10564e33b05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
