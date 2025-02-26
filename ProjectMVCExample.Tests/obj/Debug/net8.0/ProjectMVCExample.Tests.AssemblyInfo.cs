08eb1b0d9c93650918a243f675d3cd1d73ba5474
John Doe <johndoe@example.com>
2025-02-26 12:39:53 -0500

    updating2

--- ProjectMVCExample.Tests/obj/Debug/net8.0/ProjectMVCExample.Tests.AssemblyInfo.cs
+++ ProjectMVCExample.Tests/obj/Debug/net8.0/ProjectMVCExample.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVCExample.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a9b41462559e52f91e7051c108323583a2211db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c395e86e07e670efd7682a8fe9fc869de189f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVCExample.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVCExample.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
