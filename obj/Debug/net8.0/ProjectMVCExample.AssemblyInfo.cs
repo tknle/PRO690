ca8a36296d0b658b5791219567df064c0fddcb2b
John Doe <johndoe@example.com>
2025-02-26 12:25:24 -0500

    keep updating

--- obj/Debug/net8.0/ProjectMVCExample.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectMVCExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e4d3f1f3d0ecd0cc29337a38c09f40541d345f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5e42bb1afccfe5cb1ae86ac584472bb4a2f1ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVCExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
